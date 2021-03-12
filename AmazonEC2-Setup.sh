e09ddb8d2d6d798a30cf020883ce2c799248de4f
Reuben Kraft <reuben.kraft@gmail.com>
2021-03-12 10:51:43 -0500

    Update AmazonEC2-Setup.sh

--- AmazonEC2-Setup.sh
+++ AmazonEC2-Setup.sh
@@ -8,10 +8,10 @@ sudo usermod -a -G docker $UNAMEX
 cd /home/$UNAMEX
 
 # clone specific branch
-#git -b branch clone https://github.com/PSUCompBio/brain-gfx-3js.git
+git -b move-to-lambda clone https://github.com/PSUCompBio/brain-gfx-3js.git
 
 # clone master branch
-git clone https://github.com/PSUCompBio/brain-gfx-3js.git
+#git clone https://github.com/PSUCompBio/brain-gfx-3js.git
 cd brain-gfx-3js
 sudo docker build  --build-arg UNAMEX=ec2-user -t clincialimg .
 cd /home/$UNAMEX
